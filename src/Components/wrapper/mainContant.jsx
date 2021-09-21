5c3d8c4bccdaf2707bc57581eef95a883145ba21
abduljamalbasha <azeezjamal6@gmail.com>
2021-09-21 21:18:46 +0530

    error fixed

--- src/Components/wrapper/mainContant.jsx
+++ src/Components/wrapper/mainContant.jsx
@@ -1,20 +1,24 @@
 /* This example requires Tailwind CSS v2.0+ */
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useLocation } from 'react-router-dom'
 import navigation from '../../config/data/navigation'
 
 export const Content = ({children }) => {
   const location = useLocation()
   const { pathname } = location
-  const activeWorkflowName = navigation.find(
+ 
+  let activeWorkflowName = {}
+  useEffect(() => {
+    activeWorkflowName = navigation.find(
     (item) => item.href === pathname,
   )
+  },[]);
   return (
     <main className="flex-1 relative overflow-y-auto focus:outline-none">
       <div className="py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
           <h1 className="text-2xl text-left font-semibold text-gray-900">
-            {activeWorkflowName.name}
+            {activeWorkflowName && activeWorkflowName.name}
           </h1>
         </div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
